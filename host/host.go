ad2aabb57707d6df9b80b521a26f743b54e670de
karan <ka7982@rit.edu>
2023-04-29 21:38:53 -0400

    host arch.

--- host/host.go
+++ host/host.go
@@ -1,8 +1,12 @@
 package host
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	ma "github.com/multiformats/go-multiaddr"
 	"net"
+	"os"
 	"p2p/peer"
 )
 
@@ -64,4 +68,101 @@ type Host interface {
 
 	// EventBus returns the hosts eventbus
 	//EventBus() event.Bus
+}
+
+// getAddrFromUser takes in user input to return a multi address or an error
+func getAddrFromUser() (ma.Multiaddr, error) {
+	for {
+		fmt.Print("Enter the server multi-address (e.g., /ip4/127.0.0.1/tcp/8000): ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		addrStr := scanner.Text()
+
+		// Parsing the entered multi-address.
+		addr, err := ma.NewMultiaddr(addrStr)
+		if err != nil {
+			fmt.Printf("Invalid multiaddress: %s, press r to retry or any other key to exit\n", err)
+			scanner.Scan()
+			input := scanner.Text()
+			if input == "r" {
+				continue
+			} else {
+
+				return nil, errors.New("exit from get Address")
+			}
+		} else {
+			return addr, nil
+		}
+	}
+}
+
+// printProtocols takes in a multi address and returns the list of protocols in the address
+func printProtocols(addr ma.Multiaddr) {
+	fmt.Printf("Protocols in Multiaddr %s are:\n", addr.String())
+	for _, protocol := range addr.Protocols() {
+		fmt.Println("PName", protocol.Name)
+	}
+}
+
+// receiveData reads data from the server and prints received messages.
+func receiveData(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+
+	for {
+		// Read a message from the server, terminated by a newline character ('\n').
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+
+		// Print the received message.
+		fmt.Print("Received message: ", msg)
+	}
+}
+
+func main() {
+
+	addr, err := getAddrFromUser()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	printProtocols(addr)
+	ipAddr, err := addr.ValueForProtocol(ma.P_IP4)
+	if err != nil {
+		fmt.Println("Could not get ipv4 address from the given multiadress")
+	}
+	tcpPort, err := addr.ValueForProtocol(ma.P_TCP)
+	if err != nil {
+		fmt.Println("Could not get TCP Port from the given multiadress")
+	}
+	peerAddr := ipAddr + ":" + tcpPort
+	fmt.Println(peerAddr)
+
+	//Resolving the TCP address
+	ipAdd, err := net.ResolveTCPAddr("tcp", peerAddr)
+	if err != nil {
+		fmt.Printf("Error resolving TCP address: %s\n", err)
+		return
+	}
+	fmt.Println(ipAdd)
+
+	// Establish a TCP connection to the server.
+	conn, err := net.DialTCP("tcp", nil, ipAdd)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer func(conn *net.TCPConn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("error: ", err.Error())
+		}
+	}(conn) // Ensure the connection is closed when the function returns.
+
+	fmt.Println("Connected to peer:", addr.String())
+
+	go receiveData(conn)
+
 }
