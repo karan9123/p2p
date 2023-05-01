b4984083e252e43e7463bd5bd14a6480702b87d4
karan <ka7982@rit.edu>
2023-05-01 08:51:50 -0400

    Updated peer with comments.

--- peer/peer.go
+++ peer/peer.go
@@ -1,3 +1,8 @@
+/**
+ * @file peer.go
+ * @brief This file contains the implementation of the Peer ID structure and related functions used in peer-to-peer (p2p) communication.
+ */
+
 package peer
 
 import (
@@ -13,6 +18,10 @@ type ID string
 
 const maxInlineKeyLength = 42
 
+// GenerateIDFromPubKey generates a peer ID from the given public key using multihash.
+// @param pubKey The public key used to generate the ID.
+// @return The generated peer ID and nil if successful, otherwise an error is returned.
+
 func GenerateIDFromPubKey(pubKey cr.PubKey) (ID, error) {
 	b, err := cr.MarshalPublicKey(pubKey)
 	if err != nil {
@@ -23,29 +32,42 @@ func GenerateIDFromPubKey(pubKey cr.PubKey) (ID, error) {
 	if len(b) <= maxInlineKeyLength {
 		alg = mh.IDENTITY
 	}
+
+	// Generate the multihash of the public key.
 	hash, _ := mh.Sum(b, alg, -1)
 	return ID(hash), nil
 }
 
-// CheckPublicKey checks whether this ID was derived from pubKey.
+// CheckPublicKey verifies whether the peer ID was derived from the given public key.
+// @param id The peer ID to check.
+// @param pubKey The public key to compare against the ID.
+// @return True if the ID was derived from the given public key, otherwise false.
 func (id ID) CheckPublicKey(pubKey cr.PubKey) bool {
+	// Generate the peer ID using the provided public key.
 	oid, err := GenerateIDFromPubKey(pubKey)
 	if err != nil {
+		// Could not generate a public key from the given ID.
 		fmt.Println("Could not generate Public Key from: ", id)
 		return false
 	}
 	return oid == id
 }
 
-// ExtractPublicKey attempts to extract the public key from an ID.
+// ExtractPublicKey attempts to extract the public key from a given peer ID.
+// @param id The peer ID to extract the public key from.
+// @return The extracted public key and nil if successful, otherwise an error is returned.
 func (id ID) ExtractPublicKey() (cr.PubKey, error) {
 	decoded, err := mh.Decode([]byte(id))
 	if err != nil {
 		return nil, err
 	}
+
+	// Verify that the given ID contains a public key.
 	if decoded.Code != mh.IDENTITY {
 		return nil, errors.New(string("No Public Key for Peer:" + id))
 	}
+
+	// Extract the public key from the given ID.
 	pk, err := cr.UnmarshalPublicKey(decoded.Digest)
 	if err != nil {
 		return nil, err
